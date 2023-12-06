1b2748a3651b6ad1a16a038566526e08c14cb944
maskeynihal <26411488+maskeynihal@users.noreply.github.com>
2023-12-06 08:41:11 +0545

    chore: Add react-query package

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import client from './config/reactQuery';
 import router from './routes/router';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider } from '@tanstack/react-router';
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={client}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
