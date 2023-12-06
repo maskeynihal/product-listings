c98d499fce06c6f80726eda93ade6c64adba81a8
maskeynihal <26411488+maskeynihal@users.noreply.github.com>
2023-12-06 15:16:37 +0545

    feat: add button to create new beer in navbar

--- src/components/organisms/Navbar/Navbar.tsx
+++ src/components/organisms/Navbar/Navbar.tsx
@@ -1,17 +1,41 @@
-import { allBeers } from '../../../routes/routes';
+import { FormModal } from '../../../pages/MyBeers/MyBeers';
+import router from '../../../routes/router';
+import { allBeers, myBeers } from '../../../routes/routes';
 import Link from '../../atoms/Link/Link';
+import { useState } from 'react';
 
 function Navbar() {
+  const currentPath = router.state.location.pathname;
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
-    <>
-      <Link
-        to={allBeers.to}
-        className="mr-4"
-      >
-        All Beers
-      </Link>
-      <Link to="/my-beers">My Beers</Link>
-    </>
+    <div className="flex justify-between">
+      <nav>
+        <Link
+          to={allBeers.to}
+          className="mr-4"
+        >
+          All Beers
+        </Link>
+        <Link to={myBeers.to}>My Beers</Link>
+      </nav>
+
+      {currentPath === myBeers.to && (
+        <div className="">
+          <button
+            className="rounded bg-blue-400 hover:bg-blue-600 text-white px-4 py-2"
+            onClick={() => setIsModalOpen(true)}
+          >
+            Add a new beer
+          </button>
+          <FormModal
+            isOpen={isModalOpen}
+            setModalClose={() => setIsModalOpen(false)}
+          />
+        </div>
+      )}
+    </div>
   );
 }
 
--- src/pages/MyBeers/MyBeers.tsx
+++ src/pages/MyBeers/MyBeers.tsx
@@ -8,13 +8,27 @@ import { Fragment, useRef } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 
 export interface FormModal {
-  children: React.ReactNode;
   isOpen: boolean;
   setModalClose: () => void;
 }
 
-function FormModal({ children, isOpen, setModalClose }: FormModal) {
+export function FormModal({ isOpen, setModalClose }: FormModal) {
   const cancelButtonRef = useRef(null);
+  type Inputs = {
+    name: string;
+    genre: string;
+    description: string;
+  };
+
+  const { register, handleSubmit } = useForm<Inputs>();
+
+  const createMyBeerMutation = useCreateMyBeers();
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    createMyBeerMutation.mutate({ ...data, imageUrl: appConfig.image.defaultBeerImageUrl });
+
+    setModalClose();
+  };
 
   return (
     <>
@@ -53,7 +67,75 @@ function FormModal({ children, isOpen, setModalClose }: FormModal) {
               >
                 <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
                   <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-                    <div className="sm:flex sm:items-start">{children}</div>
+                    <div className="sm:flex sm:items-start">
+                      {' '}
+                      <div className="w-full">
+                        <h2 className="text-xl font-bold mb-4">Add a new beer</h2>
+
+                        <div className="w-20 h-20 border mb-4">
+                          <img
+                            src={appConfig.image.defaultBeerImageUrl}
+                            className="object-contain w-20 h-20 p-2"
+                          />
+                        </div>
+                        <form
+                          className="w-full"
+                          onSubmit={handleSubmit(onSubmit)}
+                        >
+                          <div className="mb-5">
+                            <label
+                              htmlFor="name"
+                              className="block mb-2 text-sm font-medium text-gray-900 "
+                            >
+                              Beer Name <span className="text-red-400">*</span>
+                            </label>
+                            <input
+                              type="text"
+                              id="name"
+                              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+                              required
+                              {...register('name', { required: true })}
+                            />
+                          </div>
+                          <div className="mb-5">
+                            <label
+                              htmlFor="genre"
+                              className="block mb-2 text-sm font-medium text-gray-900 "
+                            >
+                              Genre <span className="text-red-400">*</span>
+                            </label>
+                            <input
+                              type="text"
+                              id="genre"
+                              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+                              required
+                              {...register('genre', { required: true })}
+                            />
+                          </div>
+                          <div className="mb-5">
+                            <label
+                              htmlFor="genre"
+                              className="block mb-2 text-sm font-medium text-gray-900 "
+                            >
+                              Description <span className="text-red-400">*</span>
+                            </label>
+                            <textarea
+                              id="description"
+                              rows={4}
+                              className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+                              placeholder="Write description"
+                              {...register('description', { required: true })}
+                            ></textarea>
+                          </div>
+                          <button
+                            type="submit"
+                            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center "
+                          >
+                            Submit
+                          </button>
+                        </form>
+                      </div>
+                    </div>
                   </div>
                   <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
                     <button
@@ -72,91 +154,6 @@ function FormModal({ children, isOpen, setModalClose }: FormModal) {
         </Dialog>
       </Transition.Root>
     </>
-  );
-}
-
-function AddBeerForm() {
-  type Inputs = {
-    name: string;
-    genre: string;
-    description: string;
-  };
-
-  const { register, handleSubmit } = useForm<Inputs>();
-
-  const createMyBeerMutation = useCreateMyBeers();
-
-  const onSubmit: SubmitHandler<Inputs> = (data) => {
-    createMyBeerMutation.mutate({ ...data, imageUrl: appConfig.image.defaultBeerImageUrl });
-  };
-
-  return (
-    <div className="w-full">
-      <h2 className="text-xl font-bold mb-4">Add a new beer</h2>
-
-      <div className="w-20 h-20 border mb-4">
-        <img
-          src={appConfig.image.defaultBeerImageUrl}
-          className="object-contain w-20 h-20 p-2"
-        />
-      </div>
-      <form
-        className="w-full"
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <div className="mb-5">
-          <label
-            htmlFor="name"
-            className="block mb-2 text-sm font-medium text-gray-900 "
-          >
-            Beer Name <span className="text-red-400">*</span>
-          </label>
-          <input
-            type="text"
-            id="name"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
-            required
-            {...register('name', { required: true })}
-          />
-        </div>
-        <div className="mb-5">
-          <label
-            htmlFor="genre"
-            className="block mb-2 text-sm font-medium text-gray-900 "
-          >
-            Genre <span className="text-red-400">*</span>
-          </label>
-          <input
-            type="text"
-            id="genre"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
-            required
-            {...register('genre', { required: true })}
-          />
-        </div>
-        <div className="mb-5">
-          <label
-            htmlFor="genre"
-            className="block mb-2 text-sm font-medium text-gray-900 "
-          >
-            Description <span className="text-red-400">*</span>
-          </label>
-          <textarea
-            id="description"
-            rows={4}
-            className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
-            placeholder="Write description"
-            {...register('description', { required: true })}
-          ></textarea>
-        </div>
-        <button
-          type="submit"
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center "
-        >
-          Submit
-        </button>
-      </form>
-    </div>
   );
 }
 
@@ -185,16 +182,7 @@ function MyBeers() {
         <FormModal
           isOpen={isModalOpen}
           setModalClose={() => setIsModalOpen(false)}
-        >
-          <AddBeerForm />
-        </FormModal>
-
-        {/* <Modal
-          isOpen={isModalOpen}
-          contentLabel="Minimal Modal Example"
-        >
-          <AddBeerForm />
-        </Modal> */}
+        />
       </div>
     );
   }
